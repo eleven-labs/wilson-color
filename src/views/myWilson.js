14bd00d5ccb2255773231abccb8db551d022ea94
Jean-Francois <jeanf.moreno@gmail.com>
2017-10-20 11:01:38 +0200

    feat(share): add a back button in the share view

--- src/views/myWilson.js
+++ src/views/myWilson.js
@@ -8,32 +8,38 @@ export default function homeView(state, emit) {
   const url = window.location.href
 
   const view = html`
-    <div class="my-wilson">
-      <img src="images/${state.myWilson.uid}.png" />
-        <div class="infos">
-          <div class="author">créé par ${state.myWilson.name || 'Wilson'}</div>
-          <div class="vote-container">
-            <button
-              id="${state.myWilson.uid}"
-              class="btn-vote g-recaptcha">
-              <img class="votes" src='images/vote.png'/>
-            </button>
-            <span>${state.myWilson.vote || '0'}</span>
+    <div>
+      <div class="home-nav">
+        <a class="btn" href="/">Retour</a>
+      </div>
+      <div class="my-wilson">
+        <img src="images/${state.myWilson.uid}.png" />
+          <div class="infos">
+            <div class="author">créé par ${state.myWilson.name ||
+              'Wilson'}</div>
+            <div class="vote-container">
+              <button
+                id="${state.myWilson.uid}"
+                class="btn-vote g-recaptcha">
+                <img class="votes" src='images/vote.png'/>
+              </button>
+              <span>${state.myWilson.vote || '0'}</span>
+            </div>
           </div>
-        </div>
-        <div>
-          <a
-            class="fb-xfbml-parse-ignore"
-            target="_blank"
-            href="https://www.facebook.com/sharer/sharer.php?u=${url}">
-              <img width="20" height="20" src="/images/facebook.svg">
-          </a>
-          <a
-            target="_blank"
-            href="https://twitter.com/intent/tweet?text=${url}">
-              <img src="/images/twitter.svg" width="20" height="20"/>
-          </a>
-        </div>
+          <div>
+            <a
+              class="fb-xfbml-parse-ignore"
+              target="_blank"
+              href="https://www.facebook.com/sharer/sharer.php?u=${url}">
+                <img width="20" height="20" src="/images/facebook.svg">
+            </a>
+            <a
+              target="_blank"
+              href="https://twitter.com/intent/tweet?text=${url}">
+                <img src="/images/twitter.svg" width="20" height="20"/>
+            </a>
+          </div>
+      </div>
     </div>
   `
   onload(view, init)
